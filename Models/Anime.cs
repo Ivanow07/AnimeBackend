a4a59e3e41a38f0a86c4378af270bf3838a989d2
Ivanow07_ <138402069+Ivanow07@users.noreply.github.com>
2024-06-07 23:16:29 +0300

    Create Anime.cs

---
+++ Models/Anime.cs
@@ -0,0 +1,9 @@
+namespace AnimeBackend.Models
+{
+    public class Anime
+    {
+        public int AnimeId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
